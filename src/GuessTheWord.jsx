b139c46ede43310fc54d37ca328d2dcac0b76c37
Jose Escribano <jescribanomora@gmail.com>
2024-02-19 22:51:47 +0100

    feat: implement change teams funcionality

--- src/GuessTheWord.jsx
+++ src/GuessTheWord.jsx
@@ -3,33 +3,55 @@ import { useState, useEffect } from "react";
 // import { Words } from "./components/Words";
 
 export const GuessTheWord = () => {
+    // Game
     const [isPlaying, setIsPlaying] = useState(false);
-    const initialTimer = 2;
+
+    const initGame = () => {
+        setIsPlaying(true);
+        setIsRunning(true);
+    };
+
+    const stopGame = () => {
+        setIsPlaying(false);
+        setIsRunning(false);
+        setIsRunning(false);
+        stopTimer();
+        resetTimer();
+    };
+
+    // Timer
+    const initialTimer = 45;
     const [timer, setTimer] = useState(initialTimer);
     const [isRunning, setIsRunning] = useState(false);
-    const [teams, setTeams] = useState(["Team 1", "Team 2"]);
-    const [currentTeam, setCurrentTeam] = useState(teams[0]);
-    const [turns, setTurns] = useState([1, 2, 3]);
-    const [currentTurn, setCurrentTurn] = useState(turns[0]);
+    const [intervalID, setIntervalID] = useState(0);
 
     useEffect(() => {
         if (timer > 0 && isRunning) {
-            setTimeout(() => {
+            let interval = setTimeout(() => {
                 setTimer(timer - 1);
             }, 1000);
+
+            setIntervalID(interval);
         } else if (timer == 0 && isRunning) {
             changeTeam();
         }
     }, [timer, isRunning]);
 
-    const toggleTimer = (runTimer) => {
-        setIsRunning(runTimer);
+    const resumeTimer = () => {
+        setIsRunning(true);
+    };
+
+    const stopTimer = () => {
+        clearInterval(intervalID);
+        setIsRunning(false);
     };
 
     const resetTimer = () => {
+        clearInterval(intervalID);
         setTimer(initialTimer);
     };
 
+    // Words
     const currentWord = 0;
     const [words, setWords] = useState([
         "Silent Hill",
@@ -37,6 +59,12 @@ export const GuessTheWord = () => {
         "Half Life",
     ]);
     const [guessedWords, setGuessedWords] = useState([]);
+
+    useEffect(() => {
+        if (!words.length) {
+            nextTurn();
+        }
+    }, [words]);
 
     const wordIsCorrect = () => {
         const updatedWords = [...words];
@@ -65,17 +93,9 @@ export const GuessTheWord = () => {
         setWords(updatedWords);
     };
 
-    const initGame = () => {
-        setIsPlaying(true);
-        setIsRunning(true);
-    };
-
-    const stopGame = () => {
-        setIsPlaying(false);
-        setIsRunning(false);
-        setIsRunning(false);
-        resetTimer();
-    };
+    // Teams
+    const [teams, setTeams] = useState(["Team 1", "Team 2"]);
+    const [currentTeam, setCurrentTeam] = useState(teams[0]);
 
     const changeTeam = () => {
         let updatedTeams = [...teams];
@@ -88,43 +108,50 @@ export const GuessTheWord = () => {
         stopGame();
     };
 
+    // Turn
+    const totalTurns = 3;
+    const [currentTurn, setCurrentTurn] = useState(1);
+
+    const nextTurn = () => {
+        if (currentTurn == totalTurns) return false;
+
+        stopGame();
+        changeTeam();
+        setCurrentTurn(currentTurn + 1);
+    };
+
     return (
         <>
             <h1>Guess the Word - App</h1>
 
             <div>
+                {/* Turn */}
+                Turn: {currentTurn}
+                <hr />
                 {/* Team */}
                 {currentTeam}
                 <hr />
-
                 {/* Play */}
                 {JSON.stringify(isPlaying)}
-
                 <br />
                 <br />
-
                 <div>
                     <button onClick={initGame}>Play!</button>
                 </div>
-
                 <hr />
-
                 {/* Timer */}
                 <h2>{timer}</h2>
-                <button onClick={() => toggleTimer(!isRunning)}>
-                    {isRunning ? (
-                        <span>Stop timer</span>
-                    ) : (
-                        <span>Start timer</span>
-                    )}
+                <button onClick={stopTimer}>
+                    <span>Stop timer</span>
                 </button>
-
+                <button onClick={resumeTimer}>
+                    <span>Resume timer</span>
+                </button>
                 <hr />
-
                 {/* Words */}
-
                 <p>{words[currentWord]}</p>
-
+                <p>{JSON.stringify(words)}</p>
+                <p>{JSON.stringify(guessedWords)}</p>
                 {words.length && (
                     <div>
                         <button onClick={wordIsCorrect}>Correct!</button>
