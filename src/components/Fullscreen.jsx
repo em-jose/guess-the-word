b11c7810f173df8aa581b0b520005735f8ef919e
Jose Escribano <jescribanomora@gmail.com>
2024-02-20 17:14:07 +0100

    chore: install FontAwesome

--- src/components/Fullscreen.jsx
+++ src/components/Fullscreen.jsx
@@ -1,17 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpand } from "@fortawesome/free-solid-svg-icons";
+
 export const Fullscreen = () => {
+    const handleFullScreen = () => {
+        // document.body.requestFullscreen();
+        // document.exitFullscreen();
+    };
+
     return (
         <div className="my-5">
             <button
-                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-3"
-                onClick={() => document.body.requestFullscreen()}
+                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded mr-3"
+                onClick={handleFullScreen}
             >
-                Enter full screen
-            </button>
-            <button
-                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-                onClick={() => document.exitFullscreen()}
-            >
-                Exit full screen
+                <FontAwesomeIcon icon={faExpand} />
             </button>
         </div>
     );
