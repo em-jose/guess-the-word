539499b5862a277ca4cdd5eefd889ee757e467c5
Jose Escribano <jescribanomora@gmail.com>
2024-03-07 14:01:53 +0100

    chore: add elements to wordsDecks

--- src/utils/wordsDeck.js
+++ src/utils/wordsDeck.js
@@ -1,5 +1,5 @@
 export const wordsDeck = [
-    ["Silent Hill", "Dark Souls", "Half Life"],
-    ["2001: A Space Odyssey", "Hereditary", "Lost Highway"],
-    ["Pink Floyd", "The Weeknd", "Sleep Token"],
+    ["Silent Hill", "Dark Souls", "Half Life", "Nuclear Throne"],
+    ["2001: A Space Odyssey", "Hereditary", "Lost Highway", "Shin Godzilla"],
+    ["Pink Floyd", "The Weeknd", "Sleep Token", "King Crimson"],
 ];
