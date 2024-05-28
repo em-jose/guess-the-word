62a52bcfb9becf65c89c92b658399cb031f36c13
Jose Escribano <jescribanomora@gmail.com>
2024-05-28 12:44:24 +0200

    feat: create rounds objects

---
+++ src/data/rounds.ts
@@ -0,0 +1,23 @@
+export const gameRounds = [
+    {
+        id: "round1",
+        playingTeam: null,
+        rules: ["lorem", "ipsum", "dolor"],
+        points: { red: null, blue: null },
+        canSkipWord: false,
+    },
+    {
+        id: "round2",
+        playingTeam: null,
+        rules: ["lorem", "ipsum", "dolor"],
+        points: { red: null, blue: null },
+        canSkipWord: true,
+    },
+    {
+        id: "round3",
+        playingTeam: null,
+        rules: ["lorem", "ipsum", "dolor"],
+        points: { red: null, blue: null },
+        canSkipWord: true,
+    },
+];
